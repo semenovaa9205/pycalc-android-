4ba6add406378a263b609d20c104e5ef38897fae
Darksaid <74002782+semenovaa9205@users.noreply.github.com>
2022-01-25 12:37:56 +0300

    calc
    calc for android

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+class mainApp(App):
+     def runAPP(self):
+        main_Layout=BoxLayout(orientation="vertical",_Padding=10,spacing=10 )
+        return main_Layout
+     if __name__=='___main___':
+            runAPP.run()
+            
+ 
